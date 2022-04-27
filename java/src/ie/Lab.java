748c610c8136e7111e8186b2f41cbf8104e4843f
StephenHughes <StephenHughesxx@outlook.com>
2022-04-27 15:45:38 +0100

    Labx

---
+++ java/src/ie/Lab.java
@@ -0,0 +1,12 @@
+package ie;
+
+public class Lab {
+    
+    private boolean hab;
+    private String displayname;
+    private float distance;
+    private float xG;
+    private float yG;
+    private float zG;
+    private float absMag;
+}
